b9496a64e82f0cdd52e2d9ef2127ad79997c56ac
gabrielepetralia <gabri.petralia03@gmail.com>
2023-04-13 23:52:56 +0200

    [add] static markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
 const { createApp } = Vue;
+const root = document.querySelector(':root');
 
 createApp({
   data() {
-    
+    return {
+      images: [
+        "01.webp",
+        "02.webp",
+        "03.webp",
+        "04.webp",
+        "05.webp",
+      ],
+      numThumbs : 0,
+    }
   },
+
+  methods: {
+    setThumbsHeight(){
+      this.numThumbs = this.images.length;
+      root.style.setProperty("--numThumbs", this.numThumbs)
+    }
+  },
+
+  mounted(){
+    this.setThumbsHeight()
+  }
 }).mount("#app");
+
+
